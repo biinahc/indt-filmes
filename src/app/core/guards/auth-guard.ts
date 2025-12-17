71e34191d43212179cf28bb82a568e6c3981b25c
O-orion <lucas.lvtorres@gmail.com>
2025-12-17 16:23:01 -0400

    Desafio guard

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -5,13 +5,19 @@ import { AuthService } from '../../shared/services/auth-service';
 export const authGuard: CanActivateFn = (route, state) => {
   
   const authService = inject(AuthService);
-  const router = inject(Router);
+  
+  // Descomente a linha abaixo para injetar o Router
+  //const router = inject(Router);
 
-  if (authService.authenticated()) {
-    return true;
+  // Complete a lógica do guard aqui, por exemplo: chame o método isAuthenticated do AuthService
+  if () {
+    // Se o usuário estiver autenticado retorne true, exemplo: return true;
   }
 
-  router.navigate([''])
-  return false;
+  // Descomente as linhas abaixo para redirecionar o usuário se ele não estiver autenticado
+ // router.navigate([''])
+
+ // retorne false se o usuário não estiver autenticado
+  // return false;
 
 };
--- src/app/core/guards/public-guard.ts
+++ src/app/core/guards/public-guard.ts
@@ -5,13 +5,16 @@ import { AuthService } from "../../shared/services/auth-service";
 export const publicGuard: CanActivateFn = (route, state) => {
 
     const authService = inject(AuthService);
-    const router = inject(Router);
+    
+    // injet o Router para redirecionar o usuário se ele estiver autenticado
 
-    if (authService.authenticated()) {
+
+    // Complete a lógica do guard aqui, por exemplo: chame o método isAuthenticated do AuthService
+    if () {
         router.navigate(['home'])
-        return false
+        // Se o usuário estiver autenticado retorne false
     }
 
-    return true;
+    // Se o usuário não estiver autenticado retorne true
 
 }
